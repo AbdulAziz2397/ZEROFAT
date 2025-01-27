ca9b9096ad72b17979beb349996795114c081a6f
AbdulAziz2397 <abdulaziztahir2397@gmail.com>
2025-01-27 19:15:32 +0500

    zerofat

---
+++ app.js
@@ -0,0 +1,6 @@
+// var barBtn = document.getElementById("menu-bar-checkbox").addEventListener('click', menu-bar-checkboxclicked);
+
+// function menu-bar-checkboxclicked(){
+//     document.getElementsByClassName("menu").style.display = 'block';
+//     document.getElementsByClassName("menu").style.display = 'none';
+// }
